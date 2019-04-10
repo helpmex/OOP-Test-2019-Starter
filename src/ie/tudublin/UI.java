7f0a37942bf1174bc6fb61483a7a3500e5f17d43
John Doe <C17363071@mydit.ie>
2019-04-10 17:44:27 +0100

    commit test answer

--- src/ie/tudublin/UI.java
+++ src/ie/tudublin/UI.java
@@ -26,6 +26,35 @@ public class UI extends PApplet
 		separate(1);
 		separate(92);
 	}
+	Table table;
+	public void loadColours() {
+		table = loadColours("colours.csv", "header");
+		
+		println(table.getRowCount() + "total rows in table" );
+		
+		for (TableRow row : table.rows()) {
+			
+			String colour = row.getString("colour");
+			int r = row.getInt("r");
+			int g = row.getInt("g");
+			int b = row.getInt("b");
+			int value = row.getInt("value");
+		}
+	}
+	
+	public void printColours() {
+		printColours.loadColours();
+		println(colour + " has an r value of " + r " and a green value of " + g " and a blue value of " + b " and value of " + value );
+	}
+	
+	public Color findColor(int value) {
+		int val = 0;
+		val <=11;
+		if(value equals value ) {
+			println("%d the value is equal to the colour %s" value, colour );
+		}
+	}
+	
 
 	public void setup() 
 	{
@@ -34,4 +63,7 @@ public class UI extends PApplet
 	public void draw()
 	{			
 	}
+	
+	   private ArrayList<Colour> colours = new ArrayList<Colour>();
+	   private ArrayList<Resistor> resistors = new ArrayList<Resistor>();
 }
